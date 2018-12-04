44c037f480be433a99536083e2774a3111e26e88
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-12-04 13:11:16 +0000

    update

--- src/pt/ipleiria/estg/dei/aed/modelo/contactos/Contacto.java
+++ src/pt/ipleiria/estg/dei/aed/modelo/contactos/Contacto.java
@@ -5,15 +5,21 @@ public class Contacto {
     private String primeiroNome;
     private String ultimoNome;
     private long numeroTelefone;
+    private int id;
     private String morada;
     private Data dataNascimento;
 
-    public Contacto(String primeiroNome, String ultimoNome, long numeroTelefone, String morada, Data dataNascimento) {
+    public Contacto(int id,String primeiroNome, String ultimoNome, long numeroTelefone, String morada, Data dataNascimento) {
+        this.id=id;
         this.primeiroNome = primeiroNome;
         this.ultimoNome = ultimoNome;
         this.numeroTelefone = numeroTelefone;
         this.morada = morada;
         this.dataNascimento = dataNascimento;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getPrimeiroNome() {
---
+++ src/pt/ipleiria/estg/dei/aed/modelo/contactos/GestorContactos.java
@@ -0,0 +1,25 @@
+package pt.ipleiria.estg.dei.aed.modelo.contactos;
+
+import pt.ipleiria.estg.dei.aed.ComparacaoLimite;
+import pt.ipleiria.estg.dei.aed.colecoes.iteraveis.IteradorIteravel;
+import pt.ipleiria.estg.dei.aed.colecoes.iteraveis.lineares.ordenadas.estruturas.ListaDuplaCircularBaseLimiteOrdenada;
+import pt.ipleiria.estg.dei.aed.colecoes.iteraveis.lineares.ordenadas.estruturas.ListaDuplaCircularBaseLimiteOrdenadaDistinta;
+import pt.ipleiria.estg.dei.aed.modelo.contactos.comparadores.ComparacaoLimiteParesPorDataAscedente;
+import pt.ipleiria.estg.dei.aed.utils.Par;
+
+public class GestorContactos {
+    INSTANCIA;
+    private ListaDuplaCircularBaseLimiteOrdenadaDistinta<Par<Data, ListaDuplaCircularBaseLimiteOrdenada<Contacto>>>contactosPorDataNascimento;
+    GestorContactos(){
+        contactosPorDataNascimento= new ListaDuplaCircularBaseLimiteOrdenadaDistinta<>(
+                ComparacaoLimiteParesPorDataAscedente.CRITERIO
+        );
+    }
+    public void inserir(Contacto contacto){
+
+
+    }
+    public IteradorIteravel<Contacto> consultar (Data dataNascimento){
+
+    }
+}
